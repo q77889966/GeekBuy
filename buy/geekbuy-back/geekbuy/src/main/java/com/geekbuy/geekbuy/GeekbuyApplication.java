797b341db9f443a9c286dedda1f2702be6c9ee1c
Rnk0u <3345329867@qq.com>
2023-10-30 15:29:07 +0800

    v0.2

---
+++ buy/geekbuy-back/geekbuy/src/main/java/com/geekbuy/geekbuy/GeekbuyApplication.java
@@ -0,0 +1,13 @@
+package com.geekbuy.geekbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekbuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekbuyApplication.class, args);
+    }
+
+}
---
+++ buy/geekbuy-back/geekbuy/src/test/java/com/geekbuy/geekbuy/GeekbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbuy.geekbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekbuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
